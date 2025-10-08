71780b3812273c0c8bd035832df71a37f757eb90
Kicien Tania <tania.kicien@etu.umontpellier.fr>
2025-10-08 07:36:37 +0200

    wip(): Composant footer

--- src/components/Footer/Footer.styled.tsx
+++ src/components/Footer/Footer.styled.tsx
@@ -1,13 +1,29 @@
-import styled from '@emotion/styled';
+import styled from "@emotion/styled";
 
 export const FooterWrapper = styled.footer`
+    border-top: 1px solid black;
+    text-align: center;
+    position: fixed;
+    bottom: 0;
+    padding: 10px;
+    font-size: larger;
+    font-weight: bold;
     width: 100%;
-    height: 60px;
-    background-color: #333;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 14px;
-    color:  #f5f5f5;
-    border-top: 1px solid #e0e0e0;
+    height: 40px;
+    background-color: #F6CF57;
+    display: table-column;
+
+    &:hover {
+        background-color: #ffdf80;
+    }
+
+    &#infosContact {
+        display: table-row;
+    }
+    &#infosLegales {
+        display: table-row;
+    }
+    &#infosReseauxSociaux {
+        display: table-row;
+    }
 `;
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,24 +1,99 @@
 /* Global Imports */
-import React, { FC } from 'react';
+import React, { FC, PropsWithChildren } from 'react';
 
 /* Application Level Imports */
 import * as Hooks from '@/hooks';
 
 /* Local Imports */
 import { FooterWrapper } from './Footer.styled';
+import pikaUpsideDown from '@/assets/pika_upside_down.png';
+import { Link } from 'react-router';
 
+interface ReseauSocial {
+   icone: 'instagram' | 'discord' | 'facebook' | 'twitter',
+   lien: `${'https://' | 'http://'}${string}/${string}.${string}`
+   nomLien: Capitalize<string>
+}
 
-interface FooterProps { }
+interface InformationLegale {
+   lien: `/${'' | 'users'}`
+   nomLien: Capitalize<string>
+}
 
+interface InfoContact {
+   tel: number,
+   mail: string,
+   adresse: string
+}
 
-const Footer: FC<FooterProps> = () => {
+interface FooterProps extends PropsWithChildren, InfoContact {
+   liensReseauxSociaux: Array<ReseauSocial>
+   informationsLegales: Array<InformationLegale>
+}
+
+const LegalComponent:FC<InformationLegale> = ({lien, nomLien}) => (
+   <li>
+      📖
+      <Link to={lien}>
+         {nomLien}
+      </Link>
+   </li>
+)
+
+const ReseauSocialComponent:FC<ReseauSocial> = ({lien, nomLien, icone}) => (
+   <li>
+      {icone}
+      <Link to={lien} >
+         {nomLien}
+      </Link>
+   </li>
+)
+
+const Footer: FC<FooterProps> = ({children, liensReseauxSociaux, informationsLegales, tel, mail, adresse}) => {
 
    // Hooks.useGloblaEvent('click',()=> console.log('click event'));
 
    return(
-   <FooterWrapper data-testid="Footer">
-      Footer Component
-   </FooterWrapper>
+      <>
+         <div style={{position: 'fixed', bottom: '60px', textAlign: 'center', width: '100%', rotate: '180deg'}}>
+            <img src={pikaUpsideDown} width={200} height={100} />
+         </div>
+         <FooterWrapper data-testid="Footer">
+            <div id='infosContact'>
+               <h4>Informations de contact</h4>
+               <ul>
+                  <li>
+                     📞 {tel}
+                  </li>
+                  <li>
+                     📧 {mail}
+                  </li>
+                  <li>
+                     📌 {adresse}
+                  </li>
+               </ul>
+            </div>
+            <div id='infosLegales'>
+               <h4>Informations légales</h4>
+               <ul>
+                  { 
+                     informationsLegales.map((legal) => <LegalComponent lien={legal.lien} nomLien={legal.nomLien} />)
+                  }
+               </ul>
+            </div>
+            <div id='infosReseauxSociaux'>
+               <h4>Nos réseaux sociaux</h4>
+               <ul>
+                  { 
+                     liensReseauxSociaux.map((reseau) => <ReseauSocialComponent lien={reseau.lien} nomLien={reseau.nomLien} icone={reseau.icone} />)
+                  }
+               </ul>
+            </div>
+            <div id='general'>
+               {children}
+            </div>
+         </FooterWrapper>
+      </>
    );
 
 }
