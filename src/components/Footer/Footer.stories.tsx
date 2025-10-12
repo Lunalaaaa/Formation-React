375413ae9d2fb39ced311d304c62c497ab20e7f0
Kicien Tania <tania.kicien@etu.umontpellier.fr>
2025-10-12 20:06:17 +0200

    add(Footer): Footer storybook

--- src/components/Footer/Footer.stories.tsx
+++ src/components/Footer/Footer.stories.tsx
@@ -1,12 +1,57 @@
 /* eslint-disable */
-import Footer from './Footer';
+import * as UI from "@/components";
+import { Meta, StoryObj } from '@storybook/react';
+import { MemoryRouter } from "react-router";
 
-export default {
-  title: "Footer",
-};
+const meta = {
+  component: UI.Footer,
+  title: 'UI/Footer',
+  parameters: {
+    layout: 'centered'
+  },
+  argTypes: {
+    informationsLegales: {control: 'object'},
+    liensReseauxSociaux: {control: 'object'}
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof UI.Footer>;
 
-export const Default = () => <Footer />;
+export default meta;
+type Story = StoryObj<typeof meta>;
 
-Default.story = {
-  name: 'default',
+export const Standard: Story = {
+  args: {
+    adresse: "Une adresse" ,
+    informationsLegales: [
+      {
+        lien: "/",
+        nomLien: "Un lien" 
+      }
+    ],
+    liensReseauxSociaux:[
+      {
+        icone: "discord",
+        lien: "https://discord.gg",
+        nomLien: "Lien vers un discord"
+      }
+    ],
+    mail: "test@mail.com",
+    tel: "0123456789",
+  },
+  decorators: [
+  (Story) => (
+    <div style={{ 
+      height: '100vh', 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center',
+      padding: '20px',
+      boxSizing: 'border-box',
+    }}>
+      <MemoryRouter>
+        <Story />
+      </MemoryRouter>
+    </div>
+  ),
+  ],
 };
