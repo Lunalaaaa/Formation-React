05bee395742947fcc4019b47a1d5c668af322bd7
Kicien Tania <tania.kicien@etu.umontpellier.fr>
2025-10-15 19:13:28 +0200

    feat(StoryBook): StoryBook de l'image

--- src/components/Image/Image.stories.tsx
+++ src/components/Image/Image.stories.tsx
@@ -1,12 +1,38 @@
 /* eslint-disable */
-import Image from './Image';
+import * as UI from "@/components";
+import { Meta, StoryObj } from "@storybook/react";
+import pikaUpsideDown from '@/assets/pika_upside_down.png';
+import discord from '@/assets/icones/discord.png';
+import facebook from '@/assets/icones/facebook.png';
+import instagram from '@/assets/icones/instagram.png';
 
-export default {
-  title: "Image",
+const meta = {
+  component: UI.Image,
+  title: 'UI/Image',
+  parameters: {
+    layout: 'centered'
+  },
+  argTypes: {
+    imageSrc: {control: 'select', options: [pikaUpsideDown, discord, facebook, instagram]},
+    alt: {control: "text"},
+    overlay: {control: 'number'}
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof UI.Image>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Image: Story = {
+  args: {
+    imageSrc: pikaUpsideDown,
+    alt: '',
+  },
 };
 
-export const Default = () => <Image />;
-
-Default.story = {
-  name: 'default',
+export const NotFound: Story = {
+  args: {
+    imageSrc: "",
+    alt: 'Pas d\'image mais une description :p',
+  },
 };
